67e3a934d62a4783028f94fa84d7442fee1f80c1
Puzzles65 <chitrungnguyen12@gmail.com>
2024-02-07 01:26:21 +0100

    css faulty

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import "./App.scss";
+
 import { Navbar, Home, About, Contact, Projects, Skills } from "./components";
 import styles from './style.js'
 
 function App() {
   return (
-    <div className="bg-black w-full overflow-hidden">
+    <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
 
-      <div className={`bg-black ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Home />
         </div>
       </div>
 
-      <div className={`bg-black ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Skills />
           <Projects />
